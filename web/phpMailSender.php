0218fbba7f7d8a3f08f21a4d95ba8f2a57003bd0
shi <itshilei@gmail.com>
2015-01-06 18:27:06 +0100

    Add phpMailer2

--- web/phpMailSender.php
+++ web/phpMailSender.php
@@ -1,40 +1,33 @@
 <?php
-    require("class.phpmailer.php");
+
+require_once('class.phpmailer.php');
 include("class.smtp.php"); // optional, gets called from within class.phpmailer.php if not already loaded
 
-    $mail = new PHPMailer();
+$mail = new PHPMailer();
 
-    $recipiant = $_GET["email"];
-    $message = $_GET["message"];
-
-    $mail->IsSMTP();  // telling the class to use SMTP
-    $mail->SMTPAuth   = true; // SMTP authentication
-    $mail->Host       = "smtp.hotmail.com"; // SMTP server
-    $mail->Port       = 465; // SMTP Port
-    $mail->SMTPSecure = 'ssl';
-    $mail->Username   = "apcitalia@hotmail.com"; // SMTP account username
-    $mail->Password   = "Apci2014";        // SMTP account password
+$mail->IsSMTP(); // telling the class to use SMTP
+$mail->Host = "mail.hotmail.com"; // SMTP server
+$mail->SMTPDebug = 1;                     // enables SMTP debug information (for testing)
+// 1 = errors and messages
+// 2 = messages only
 
 
-
-    $mail->SetFrom('apcitalia@hotmail.com', 'apci-italia.it'); // FROM
-    $mail->AddReplyTo('acpitaly@gmail.com', 'APCI'); // Reply TO
-
-    $mail->AddAddress($recipiant, ''); // recipient email
-
-    $mail->Subject    = "Registration Mail"; // email subject
-    $mail->Body       = $message;
+$mail->SetFrom('apcitalia@hotmail.com', 'apci italia'); // FROM
+$mail->AddReplyTo('acpitaly@gmail.com', 'APCI'); // Reply TO
 
 
+$mail->Subject = "APCI Registration Mail";
 
-    if(!$mail->Send()) {
-      echo 'Message was not sent.';
-      echo 'Mailer error: ' . $mail->ErrorInfo;
-    } else {
-        echo 'Message has been sent.';
-    }
-    
+$mail->Body = $message;
+
+$recipiant = $_GET["email"];
+$mail->AddAddress($recipiant, "jone nash");
 
 
+if (!$mail->Send()) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+} else {
+    echo "Message sent!";
+}
 ?>
     
