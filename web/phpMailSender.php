770f6335ef66d006ca583d05f0da4605d343b3d3
shi <itshilei@gmail.com>
2015-01-06 20:26:47 +0100

    Add phpMailer8

--- web/phpMailSender.php
+++ web/phpMailSender.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
   require_once('class.phpmailer.php');
   include("class.smtp.php"); // optional, gets called from within class.phpmailer.php if not already loaded
@@ -36,25 +37,28 @@
 
 
 
-/* These will gather what the user has typed into the fieled. */
 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$question = $_POST['question'];
 
-/* These are the variable that tell the subject of the email and where the email will be sent. */
 
-$emailSubject = 'Recupero password LightSchool';
-$mailto = $_POST['email'];
-$mail->From = 'apcitalia@hotmail.com';
-$mail->FromName = 'Mailer';
+require("class.phpmailer.php");
+include("class.smtp.php"); // optional, gets called from within class.phpmailer.php if not already loaded
 
-/* This takes the information and lines it up the way you want it to be sent in the email. */
+$mail = new PHPMailer();
 
-$body = '<br><hr><br> Name: ' . $name . ' <br> Email: ' . $email . ' <br> Question: ' . $question . ' <br>';
+$mail->IsSMTP();  // telling the class to use SMTP
+$mail->Host = "smtp.hotmail.com"; // SMTP server
 
-$headers = "From: $email\r\n"; // This takes the email and displays it as who this email is from.
-$headers .= "Content-type: text/html\r\n"; // This tells the server to turn the coding into the text.
-$success = mail($mailto, $emailSubject, $body, $headers); // This tells the server what to send.
+$mail->From = "apcitalia@hotmail.com";
+$mail->AddAddress("acpitaly@gmail.com");
+
+$mail->Subject = "An HTML Message";
+$mail->Body = "Hello, <b>my friend</b>! \n\n This message uses HTML entities!";
+
+
+  if (!$mail->Send()) {
+  echo "Mailer Error: " . $mail->ErrorInfo;
+  } else {
+  echo "Message sent!";
+  }
 ?>
-
+    
